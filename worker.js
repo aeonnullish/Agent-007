ed2c25b6220d919e814dc4aef7b91e65120d7465
aeonnullish <aeonnull@icloud.com>
2026-02-27 23:53:49 +0100

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,26 @@
+export default {
+  async fetch(request) {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        }
+      });
+    }
+    const body = await request.text();
+    const r = await fetch('https://signet.opnet.org', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: body
+    });
+    const data = await r.text();
+    return new Response(data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
+}
